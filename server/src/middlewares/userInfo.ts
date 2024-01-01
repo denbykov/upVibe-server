be759717328b074f61b48fdb448a283a9e3df2f8
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2024-01-01 21:29:46 +0200

    Update userinfo.ts

--- server/src/middlewares/userInfo.ts
+++ server/src/middlewares/userInfo.ts
@@ -2,28 +2,26 @@ import { NextFunction, Request, Response } from 'express';
 
 import { Config } from '@src/entities/config';
 import { Response as responseServer } from '@src/entities/response';
-import { User } from '@src/entities/user';
-import { parseReadableStreamToJSON } from '@src/utils/server/parseReadableStreamToJSON';
+
+// import { User } from '@src/entities/user';
+// import { readJSON } from '@src/utils/server/readJSON';
 
 const userInfoMiddleware = (config: Config, permissions: Array<string>) => {
-  const domain = config.autho0Domain;
+  // const domain = config.autho0Domain;
   return async (request: Request, response: Response, next: NextFunction) => {
     const headers = new Headers();
     headers.append('Authorization', request.headers.authorization || '');
-    const req = await fetch(`https://${domain}/userinfo`, {
-      headers: headers,
-    });
+    // const req = await fetch(`https://${domain}/userinfo`, {
+    //   headers: headers,
+    // });
 
-    const reqData =
-      req.status == 200 ? await parseReadableStreamToJSON(req.body!) : {};
-    const pureToken = request.headers.authorization
-      ?.split(' ')[1]
-      .split('.')[1];
-    const convertedToken = JSON.parse(
-      Buffer.from(pureToken!, 'base64').toString('ascii')
+    // const reqData = req.status == 200 ? await readJSON(req.body!) : {};
+    const rawToken = request.headers.authorization?.split(' ')[1].split('.')[1];
+    const token = JSON.parse(
+      Buffer.from(rawToken!, 'base64').toString('ascii')
     );
     for (const permission of permissions) {
-      if (!convertedToken.permissions.includes(permission)) {
+      if (!token.permissions.includes(permission)) {
         const message = new responseServer(
           responseServer.Code.Forbidden,
           'You do not have permission to access this resource.',
@@ -32,7 +30,7 @@ const userInfoMiddleware = (config: Config, permissions: Array<string>) => {
         return response.status(message.httpCode).send(message.serialize());
       }
     }
-    request.body.user = new User(reqData?.sub, reqData?.username);
+    // request.body.user = new User(reqData?.sub, reqData?.username);
     next();
   };
 };
