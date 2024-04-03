aab22f568feb4c2da0c8f65777b9a71f5626567f
Denys Bykov <53918288+denbykov@users.noreply.github.com>
2024-04-03 18:38:13 +0300

    Uvc 8 (#76)

--- server/src/middlewares/userManagementMiddleware.ts
+++ server/src/middlewares/userManagementMiddleware.ts
@@ -7,15 +7,19 @@ const userManagementMiddleware = (
   worker: UserWorker
 ) => {
   return async (request: Request, response: Response, next: NextFunction) => {
-    const rawToken = request.headers.authorization!.split(' ')[1];
-    const encodedTokenPayload = rawToken.split('.')[1];
-    const tokenPayload: JSON.JSONObject = JSON.parse(
-      Buffer.from(encodedTokenPayload!, 'base64').toString('ascii')
-    );
-    const dbUser = await worker.handleAuthorization(tokenPayload, permissions);
-    request.body.rawToken = rawToken;
-    request.body.user = dbUser;
-    next();
+    try {
+      const rawToken = request.headers.authorization!.split(' ')[1];
+      const encodedTokenPayload = rawToken.split('.')[1];
+      const tokenPayload: JSON.JSONObject = JSON.parse(
+        Buffer.from(encodedTokenPayload!, 'base64').toString('ascii')
+      );
+      
+      const dbUser = await worker.handleAuthorization(tokenPayload, permissions);
+      request.body.rawToken = rawToken;
+      request.body.user = dbUser;
+    } catch (error) {
+      next(error);
+    }
   };
 };
 
