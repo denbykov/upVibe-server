79bcfb86c4051ac811cf4eff7183998d4da367eb
denbykov <jerkinston@gmail.com>
2024-04-03 18:17:57 +0300

    Fix indentation

--- server/src/middlewares/userManagementMiddleware.ts
+++ server/src/middlewares/userManagementMiddleware.ts
@@ -7,15 +7,15 @@ const userManagementMiddleware = (
   worker: UserWorker
 ) => {
   return async (request: Request, response: Response, next: NextFunction) => {
-    const rawToken = request.headers.authorization!.split(' ')[1];
-    const encodedTokenPayload = rawToken.split('.')[1];
-    const tokenPayload: JSON.JSONObject = JSON.parse(
+      const rawToken = request.headers.authorization!.split(' ')[1];
+      const encodedTokenPayload = rawToken.split('.')[1];
+      const tokenPayload: JSON.JSONObject = JSON.parse(
       Buffer.from(encodedTokenPayload!, 'base64').toString('ascii')
     );
     try {
-    const dbUser = await worker.handleAuthorization(tokenPayload, permissions);
-    request.body.rawToken = rawToken;
-    request.body.user = dbUser;
+      const dbUser = await worker.handleAuthorization(tokenPayload, permissions);
+      request.body.rawToken = rawToken;
+      request.body.user = dbUser;
     } catch (error) {
       next(error);
     }
