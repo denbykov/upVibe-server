18cedbe260892122820b80b82e577ef84e8b71fa
Denys Bykov <53918288+denbykov@users.noreply.github.com>
2024-05-26 18:26:22 +0300

    Fix tag application issue (#112)

--- server/sql/getMappedTag.sql
+++ server/sql/getMappedTag.sql
@@ -1,55 +1,18 @@
 SELECT
-  (
-    SELECT
-      title
-    FROM
-      tags as t
-    WHERE
-      t.source = tm.title
-  ) as tag_title,
-  (
-    SELECT
-      artist
-    FROM
-      tags as t
-    WHERE
-      t.source = tm.artist
-  ) as tag_artist,
-  (
-    SELECT
-      album
-    FROM
-      tags as t
-    WHERE
-      t.source = tm.album
-  ) as tag_album,
-  (
-    SELECT
-      year
-    FROM
-      tags as t
-    WHERE
-      t.source = tm.year
-  ) as tag_year,
-  (
-    SELECT
-      track_number
-    FROM
-      tags as t
-    WHERE
-      t.source = tm.track_number
-  ) as tag_track_number,
-  (
-    SELECT
-      picture_path
-    FROM
-      tags as t
-    WHERE
-      t.source = tm.picture
-  ) as tag_picture_path
+  t1.title as tag_title,
+  t2.artist as tag_artist,
+  t3.album as tag_album,
+  t4.year as tag_year,
+  t5.track_number as tag_track_number,
+  t6.picture_path as tag_picture_path
 FROM
-  files as f
-  LEFT JOIN tag_mappings as tm ON f.id = tm.file_id
+  tag_mappings as tm
+  LEFT JOIN tags as t1 on t1.file_id = $1 and t1.source = tm.title
+  LEFT JOIN tags as t2 ON t2.file_id = $1 and t2.source = tm.artist
+  LEFT JOIN tags as t3 ON t3.file_id = $1 and t3.source = tm.album
+  LEFT JOIN tags as t4 ON t4.file_id = $1 and t4.source = tm.year
+  LEFT JOIN tags as t5 ON t5.file_id = $1 and t5.source = tm.track_number
+  LEFT JOIN tags as t6 ON t6.file_id = $1 and t6.source = tm.picture
 WHERE
-  f.id = $1
-  AND tm.user_id = $2
+  tm.file_id = $1
+  AND tm.user_id = $2;
