9e8d96afa9c9a92602baec25d524b905da50de69
Volodymyr Fihurniak <67525028+VolodymyrFihurniak@users.noreply.github.com>
2024-06-10 00:03:25 +0300

    Uvs-135: Fix a bug when processing permissions on routes (#136)

--- server/src/routes/sourceRoute.ts
+++ server/src/routes/sourceRoute.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import pg from 'pg';
 
+import { UserWorker } from '@src/business/userWorker';
 import { SourceController } from '@src/controllers';
+import { FileRepository, UserInfoAgent, UserRepository } from '@src/data';
 import { Config } from '@src/entities/config';
+import { userManagementMiddleware } from '@src/middlewares';
 import auth0Middleware from '@src/middlewares/auth0Middleware';
 import { PluginManager } from '@src/pluginManager';
 import { SQLManager } from '@src/sqlManager';
 
 import { BaseRoute } from './baseRoute';
+import { DEBUG, GENERAL } from './permissions';
 
 export class SourceRoute extends BaseRoute {
   constructor(
@@ -29,16 +33,27 @@ export class SourceRoute extends BaseRoute {
     );
 
     const sourcesURI = `/up-vibe/v1/sources`;
+    const userWorker = new UserWorker(
+      new UserRepository(this.dbPool, this.sqlManager),
+      new FileRepository(this.dbPool, this.sqlManager),
+      new UserInfoAgent(this.config)
+    );
 
     this.app.get(
       `${sourcesURI}`,
       auth0Middleware(this.config),
+      this.config.appDebug
+        ? userManagementMiddleware([GENERAL, DEBUG], userWorker, this.config)
+        : userManagementMiddleware([GENERAL], userWorker, this.config),
       controller.getSources
     );
 
     this.app.get(
       `${sourcesURI}/:sourceId/logo`,
       auth0Middleware(this.config),
+      this.config.appDebug
+        ? userManagementMiddleware([GENERAL, DEBUG], userWorker, this.config)
+        : userManagementMiddleware([GENERAL], userWorker, this.config),
       controller.getSourceLogo
     );
 
