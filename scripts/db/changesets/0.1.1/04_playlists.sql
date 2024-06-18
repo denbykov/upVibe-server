62718dbac2b64a02e825a103b499075d50140cbb
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2024-06-18 21:53:52 +0000

    Add playlist-related database tables and constraints

---
+++ scripts/db/changesets/0.1.1/04_playlists.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset VolodymyrFihurniak:4
+
+CREATE TABLE playlists (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    source_url VARCHAR(255) UNIQUE,
+    source_id INT NOT NULL,
+    added_ts TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(2) NOT NULL,
+    synchronization_ts TIMESTAMPTZ NULL,
+    title VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_playlists_sources FOREIGN KEY (source_id) REFERENCES sources(id),
+    CONSTRAINT fk_playlists_statuses FOREIGN KEY (status) REFERENCES playlist_statuses(status)
+);
+INSERT INTO playlists (source_id, status, title) VALUES (1, 'CR', 'Default');
